65584fb6a0a46bb4000e1a9cb193c9f950034b5d
unknown <happykamogelo33@gmail.com>
2021-11-09 13:36:50 +0200

    people added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const names = ["Happy", "Wandile", "Mduduzi", "Hardy", "Lebo", "Tsheiso", "Thandiwe", "Tumi", "Karabo", "Neo", "Lubanzi", "Kagiso", "Sithembiso", "Mpho", "Theo"]
+var people = ["Happy", "Wandile", "Mduduzi", "Hardy", "Lebo", "Tsheiso", "Thandiwe", "Tumi", "Karabo", "Neo", "Lubanzi"]
 const display = document.getElementById("display");
 
-for (let i = 0; i < names.length ; i++ ) {
+for (let i = 0; i < people.length ; i++ ) {
 
-display.innerHTML += "<li>" + names[i] + "</li>"
+display.innerHTML += "<li>" + people[i] + "</li>"
 }
