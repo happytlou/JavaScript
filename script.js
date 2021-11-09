a2213aa040df04610c6051085c32a405d76efc3b
unknown <happykamogelo33@gmail.com>
2021-11-09 13:47:32 +0200

    vehicle added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var people = ["Happy", "Wandile", "Mduduzi", "Hardy", "Lebo", "Tsheiso", "Thandiwe", "Tumi", "Karabo", "Neo", "Lubanzi"]
+var vehicle = ["BMW", "VW", "Toyota", "Kia", "Opel"]
 const display = document.getElementById("display");
 
-for (let i = 0; i < people.length ; i++ ) {
+for (let i = 0; i < vehicle.length ; i++ ) {
 
-display.innerHTML += "<li>" + people[i] + "</li>"
+display.innerHTML += "<li>" + vehicle[i] + "</li>"
 }
